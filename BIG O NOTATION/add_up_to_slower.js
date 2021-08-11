7b19a40e5dcd3f48a8506320738e3b07af017d90
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-11 19:53:34 +0300

    addUpTo slow performance

--- BIG O NOTATION/add_up_to_slower.js
+++ BIG O NOTATION/add_up_to_slower.js
@@ -1,3 +1,6 @@
+//Suppose we want to write a function that calculates the sum of all numbers from 1 up to (and including) some number n.
+
+
 function addUpTo(n) {
   let total = 0;
   for (let i = 1; i <= n; i++) {
