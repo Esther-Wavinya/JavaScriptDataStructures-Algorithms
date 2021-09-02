c6d30960ef3b29152f4d8304333f4ed33549dc2f
Esther Wavinya <estherwavinya80@gmail.com>
2021-09-02 15:40:27 +0300

    big o notation

--- BIG O NOTATION/bigo.js
+++ BIG O NOTATION/bigo.js
@@ -49,3 +49,16 @@ Variable assignment is constant
 Accessing elements in an array (by index) or object (by key) is constant
 In a loop,  the complexity is the length of the loop times the complexity of whatever happens inside of the loop
  */
+
+
+
+
+/*
+RECAP
+To analyze the performance of an algorithm, we use Big O Notation
+Big O Notation can give us a high level understanding of the time or space complexity of an algorithm
+Big O Notation doesn't care about precision, only about general trends (linear? quadratic? constant?)
+The time or space complexity (as measured by Big O) depends only on the algorithm, not the hardware used to run the algorithm
+Big O Notation is everywhere, so get lots of practice!
+
+*/
