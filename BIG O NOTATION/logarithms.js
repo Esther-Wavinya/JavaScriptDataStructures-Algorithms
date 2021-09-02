cf49fbe88433712cb3d990efe51741658af83315
Esther Wavinya <estherwavinya80@gmail.com>
2021-09-02 15:21:33 +0300

    Logarithmic time complexity

---
+++ BIG O NOTATION/logarithms.js
@@ -0,0 +1,27 @@
+/*
+Logarithms
+We've encountered some of the most common complexities: O(1), O(n), O(n  )
+
+2
+
+Sometimes big O expressions involve more complex mathematical expressions
+
+One that appears more often than you might like is the logarithm!
+
+
+
+Wait, what's a log again?
+The logarithm of a number roughly measures the number of times you can divide that number by 2 before you get a value that's less than or equal to one.
+
+
+Logarithmic time complexity
+Who Cares?
+Certain searching algorithms have logarithmic time complexity.
+
+Efficient sorting algorithms involve logarithms.
+
+Recursion sometimes involves logarithmic space complexity.
+
+...and more!
+
+*/
